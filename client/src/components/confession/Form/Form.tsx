e32742ef77fbebd467ed53f40231e49c3717db79
yaunga@hotmail.com <yaunga@hotmail.com>
2023-04-03 15:45:17 +0100

    finished revamp form

--- client/src/components/confession/Form/Form.tsx
+++ client/src/components/confession/Form/Form.tsx
@@ -1,17 +1,78 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import ReasonContact from '../ResonContact/ReasonContact';
 import Subject from '../Subject/Subject';
 import { API_BASE_URL } from '../../../config/config';
 
 const Form = () => {
+	const formElement = useRef(null);
+
 	const [ subjectInputValue, setSubjectInputValue ] = useState<string>('');
     const [ detailsInputValue, setDetailsInputValue ] = useState<string>('');
     const [ selectValue, setSelectValue ] = useState<string>('default');
 
-	const submitForm = async () => {
+
+	function useForm() {
+		const [status, setStatus] = useState("");
+		const [justTalked, setJustTalked] = useState("");
+		const [message, setMessage] = useState("");
+	  
+		const handleSubmit = async (e:Event) => {
+			try {
+				e.preventDefault();
+				setStatus("loading");
+				setMessage("");
+		
+				const result = await fetch(`${API_BASE_URL}/confess`, {
+				method: "POST",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ "subject": subjectInputValue, "reason": selectValue, "details": detailsInputValue }),
+				})
+
+				const status = await result.status;
+				const json = await result.json();
+				setMessage(json.message);
+
+				if (status !== 200) {
+					setStatus("error");
+				} else {
+					setStatus("success");
+				}
+			} catch (e) {
+				console.error(e);
+			}    
+
+		};
+	  
+		return { handleSubmit, status, justTalked, message };
+	}
+
+	const { handleSubmit, status, justTalked, message } = useForm();
+
+	if (status === "success") {
+		return (
+		  <>
+			<h2 className="message">Thank you!</h2>
+			<h2 className="message">{message}</h2>
+		  </>
+		);
+	}
+	
+	if (status === "error") {
+		return (
+			<>
+			<h2 className="message">Something bad happened!</h2>
+			<h2 className="message">{message}</h2>
+			</>
+		);
+	}
+
+	const submitForm = async (e:Event) => {
 
 		try {
-			
+			e.preventDefault();
 			const result = await fetch(`${API_BASE_URL}/confess`, {
 				headers: {
 					"Content-Type": "application/json",
@@ -32,7 +93,7 @@ const Form = () => {
 	return (
 		<>
 			<section className='confessionForm'>
-				<form className="form__container form form__text" onSubmit={submitForm}>
+				<form className="form__container form form__text" onSubmit={(e : any) => handleSubmit(e)} ref={formElement}>
 					<Subject subject={subjectInputValue} onChangeSubject={(e : any) => setSubjectInputValue(e.target.value)} />
 
 					<ReasonContact selectValue={selectValue} onChangeSelect={(e : any) => setSelectValue(e.target.value)} 
